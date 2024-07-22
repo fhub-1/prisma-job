a3091d3090e1651c75d75a5ade1f595c0032d46b
fhub-1 <flutterhub1.21.0@gmail.com>
2024-07-22 20:01:55 +0300

    adding igore error when build

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,5 +1,14 @@
 import { z } from "zod";
 
+// making sure the form inputs are required by default
+const requiredstring = z.string().min(1, "Required")
+
+export const createJobSchema = z.object({
+    title: requiredstring.max(100),
+    type: requiredstring
+})
+
+
 export const jobFilterSchema = z.object({
     q: z.string().optional(),
     type: z.string().optional(),
